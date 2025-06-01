5904cd1213cae5fa033433f880edc9f6a14b2ffd
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-02 02:23:38 +0300

    fix: updated port listening for test deployment

--- vendor-mock-api-gateway/cmd/api/server.go
+++ vendor-mock-api-gateway/cmd/api/server.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 	"vendor-mock-api-gateway/vendor-mock-api-gateway/internal/api/routers"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	r := routers.SetUpRouter()
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run(":" + port); err != nil {
 		log.Fatalf("Failed to start the server: %v", err)
 	}
+
 }
