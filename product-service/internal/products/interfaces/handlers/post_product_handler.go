f282ced82c559c66440a5cbf1dcf2353f411a39c
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-16 21:36:58 +0300

    feat: added handler implementation for adding new product

---
+++ product-service/internal/products/interfaces/handlers/post_product_handler.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"dev-vendor/product-service/internal/products/application/services"
+	"dev-vendor/product-service/internal/products/dtos"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+func (h *ProductHandler) PostProductHandler(c *gin.Context) {
+
+	v, _ := c.Get("vendorId")
+	vendorId, ok := v.(uuid.UUID)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid vendorId"})
+		return
+	}
+
+	var product dtos.ProductRequest
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product data"})
+		return
+	}
+
+	newProduct, err := services.PostProduct(c.Request.Context(), h.ProductRepo, product, vendorId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, newProduct)
+
+}
