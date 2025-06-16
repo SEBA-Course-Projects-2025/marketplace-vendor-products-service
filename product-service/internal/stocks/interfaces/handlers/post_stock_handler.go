56606fd7542bf359c0fca9783193f3a2dc24d165
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-16 21:50:20 +0300

    feat: added handler implementation for adding new stock

---
+++ product-service/internal/stocks/interfaces/handlers/post_stock_handler.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"dev-vendor/product-service/internal/stocks/application/services"
+	"dev-vendor/product-service/internal/stocks/dtos"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+func (h *StockHandler) PostStockHandler(c *gin.Context) {
+
+	v, _ := c.Get("vendorId")
+	vendorId, ok := v.(uuid.UUID)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid vendorId"})
+		return
+	}
+
+	var stockReq dtos.StockRequest
+
+	if err := c.ShouldBindJSON(&stockReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid stock data"})
+		return
+	}
+
+	newStock, err := services.PostStock(c.Request.Context(), h.StockRepo, h.ProductRepo, stockReq, vendorId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, newStock)
+
+}
