92883f889a777d88357534310a609a41176fe841
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-16 21:24:41 +0300

    feat: implemented a ProductRepository interface with GormProductRepository

---
+++ product-service/internal/products/infrastructure/repository/product_gorm.go
@@ -0,0 +1,146 @@
+package repository
+
+import (
+	"context"
+	"dev-vendor/product-service/internal/products/domain/models"
+	"dev-vendor/product-service/internal/products/dtos"
+	"dev-vendor/product-service/internal/shared/utils"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type GormProductRepository struct {
+	Db *gorm.DB
+}
+
+func (gpr *GormProductRepository) FindById(ctx context.Context, id uuid.UUID, vendorId uuid.UUID) (*models.Product, error) {
+
+	var product models.Product
+
+	if err := gpr.Db.WithContext(ctx).Preload("Images").Preload("Tags").First(&product, "id = ? AND vendor_id = ?", id, vendorId).Error; err != nil {
+		return nil, utils.ErrorHandler(err, "Error getting product data")
+	}
+
+	return &product, nil
+
+}
+
+func (gpr *GormProductRepository) FindAll(ctx context.Context, params dtos.ProductQueryParams, vendorId uuid.UUID) (*[]models.Product, error) {
+
+	var products []models.Product
+
+	db := gpr.Db.WithContext(ctx)
+
+	db = db.Where("vendor_id = ?", vendorId)
+
+	db = db.Preload("Images").Preload("Tags")
+
+	if params.Category != "" {
+		db = db.Where("category = ?", params.Category)
+	}
+
+	if params.MinPrice > 0 {
+		db = db.Where("price >= ?", params.MinPrice)
+	}
+
+	if params.MaxPrice > 0 {
+		db = db.Where("price <= ?", params.MaxPrice)
+	}
+
+	if params.Search != "" {
+		db = db.Where("name ILIKE ?", "%"+params.Search+"%")
+	}
+
+	allowedSortBy := map[string]bool{
+		"price":    true,
+		"name":     true,
+		"quantity": true,
+	}
+
+	orderField := "name"
+
+	if allowedSortBy[params.SortBy] {
+		orderField = params.SortBy
+	}
+
+	orderDir := "asc"
+
+	if params.SortOrder == "desc" {
+		orderDir = "desc"
+	}
+
+	db = db.Order(orderField + " " + orderDir)
+
+	if err := db.Limit(params.Limit).Offset(params.Offset).Find(&products).Error; err != nil {
+		return nil, utils.ErrorHandler(err, "Error getting paginated products data")
+	}
+
+	return &products, nil
+
+}
+
+func (gpr *GormProductRepository) Create(ctx context.Context, newProduct *models.Product, vendorId uuid.UUID) (*models.Product, error) {
+
+	newProduct.VendorId = vendorId
+
+	if err := gpr.Db.WithContext(ctx).Create(newProduct).Error; err != nil {
+		return nil, utils.ErrorHandler(err, "Error creating product")
+	}
+
+	return newProduct, nil
+
+}
+
+func (gpr *GormProductRepository) Update(ctx context.Context, updatedProduct *models.Product) error {
+
+	if err := gpr.Db.WithContext(ctx).Save(updatedProduct).Error; err != nil {
+		return utils.ErrorHandler(err, "Error updating product")
+	}
+
+	return nil
+
+}
+
+func (gpr *GormProductRepository) Patch(ctx context.Context, modifiedProduct *models.Product) (*models.Product, error) {
+
+	if err := gpr.Db.WithContext(ctx).Save(modifiedProduct).Error; err != nil {
+		return nil, utils.ErrorHandler(err, "Error updating product")
+	}
+
+	return modifiedProduct, nil
+
+}
+
+func (gpr *GormProductRepository) DeleteById(ctx context.Context, id uuid.UUID, vendorId uuid.UUID) error {
+
+	res := gpr.Db.WithContext(ctx).Where("id = ? AND vendor_id = ?", id, vendorId).Delete(&models.Product{})
+	if res.Error != nil {
+		return utils.ErrorHandler(res.Error, "Error deleting product")
+	}
+	if res.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+
+	return nil
+
+}
+
+func (gpr *GormProductRepository) DeleteMany(ctx context.Context, ids []uuid.UUID, vendorId uuid.UUID) error {
+
+	return gpr.Db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+
+		res := tx.Where("vendor_id = ? AND id IN ?", vendorId, ids).Delete(&models.Product{})
+
+		if res.Error != nil {
+			return utils.ErrorHandler(res.Error, "Error deleting product")
+		}
+
+		if res.RowsAffected == 0 {
+			return gorm.ErrRecordNotFound
+		}
+
+		return nil
+
+	})
+
+}
