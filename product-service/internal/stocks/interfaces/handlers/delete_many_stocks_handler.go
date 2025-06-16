10f3d5067bc1056261cdf8c5de4cb91d28edc088
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-16 21:47:25 +0300

    feat: added handlers implementations for deleting multiple stocks, deleting one stock by id, deleting product from stock by id and deleting many products from stock

---
+++ product-service/internal/stocks/interfaces/handlers/delete_many_stocks_handler.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"dev-vendor/product-service/internal/products/dtos"
+	"dev-vendor/product-service/internal/stocks/application/services"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func (h *StockHandler) DeleteManyStocksHandler(c *gin.Context) {
+
+	v, _ := c.Get("vendorId")
+	vendorId, ok := v.(uuid.UUID)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid vendorId"})
+		return
+	}
+
+	var ids dtos.IdsToDelete
+
+	if err := c.ShouldBindJSON(&ids); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := services.DeleteManyStocks(c.Request.Context(), h.StockRepo, ids.Ids, vendorId); err != nil {
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Stock not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{})
+
+}
---
+++ product-service/internal/stocks/interfaces/handlers/delete_stock_product_by_id_handler.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"dev-vendor/product-service/internal/stocks/application/services"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func (h *StockHandler) DeleteStockProductByIdHandler(c *gin.Context) {
+
+	v, _ := c.Get("vendorId")
+	vendorId, ok := v.(uuid.UUID)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid vendorId"})
+		return
+	}
+
+	stockIdStr := c.Param("stockId")
+	productIdStr := c.Param("productId")
+
+	stockId, err := uuid.Parse(stockIdStr)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid UUID"})
+		return
+	}
+
+	productId, err := uuid.Parse(productIdStr)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid UUID"})
+		return
+	}
+
+	if err := services.DeleteStockProductById(c.Request.Context(), h.StockRepo, stockId, productId, vendorId); err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Stock product not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{})
+
+}
