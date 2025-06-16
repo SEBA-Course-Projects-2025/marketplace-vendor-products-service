e589383ca3d053b19dd589ef09be20d245fbb8b4
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-16 21:27:16 +0300

    feat: added business logic implementation for getting all products data

---
+++ product-service/internal/products/application/services/get_all_products.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"context"
+	"dev-vendor/product-service/internal/products/domain"
+	"dev-vendor/product-service/internal/products/dtos"
+	"github.com/google/uuid"
+)
+
+func GetAllProducts(ctx context.Context, repo domain.ProductRepository, params dtos.ProductQueryParams, vendorId uuid.UUID) ([]dtos.GetProductsResponse, error) {
+
+	products, err := repo.FindAll(ctx, params, vendorId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dtos.ProductsToDto(*products), nil
+
+}
