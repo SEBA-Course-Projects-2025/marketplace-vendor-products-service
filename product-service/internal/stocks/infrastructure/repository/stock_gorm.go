b18c3c4a4e2a1e3e63a8a8a1e1ab705dcc706e63
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-21 00:35:47 +0300

    feat/fix: added implementations of the method signatures from the stock repository interface, updated previous data access layer implementations

--- product-service/internal/stocks/infrastructure/repository/stock_gorm.go
+++ product-service/internal/stocks/infrastructure/repository/stock_gorm.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	productsModels "dev-vendor/product-service/internal/products/domain/models"
 	"dev-vendor/product-service/internal/shared/utils"
+	"dev-vendor/product-service/internal/stocks/domain"
 	"dev-vendor/product-service/internal/stocks/domain/models"
 	"dev-vendor/product-service/internal/stocks/dtos"
 	"github.com/google/uuid"
@@ -11,14 +12,18 @@ import (
 )
 
 type GormStockRepository struct {
-	Db *gorm.DB
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) *GormStockRepository {
+	return &GormStockRepository{db: db}
 }
 
 func (gsr *GormStockRepository) FindById(ctx context.Context, id uuid.UUID, vendorId uuid.UUID) (*models.Stock, error) {
 
 	var stock models.Stock
 
-	if err := gsr.Db.WithContext(ctx).Preload("Location").Preload("StocksProducts.Product.Images").First(&stock, "id = ? AND vendor_id = ?", id, vendorId).Error; err != nil {
+	if err := gsr.db.WithContext(ctx).Preload("Location").Preload("StocksProducts.Product.Images").First(&stock, "id = ? AND vendor_id = ?", id, vendorId).Error; err != nil {
 		return nil, utils.ErrorHandler(err, "Error getting stock data")
 	}
 
@@ -29,7 +34,7 @@ func (gsr *GormStockRepository) FindAll(ctx context.Context, params dtos.StockQu
 
 	var stocks *[]models.Stock
 
-	db := gsr.Db.WithContext(ctx)
+	db := gsr.db.WithContext(ctx)
 
 	db = db.Where("vendor_id = ?", vendorId)
 
@@ -58,7 +63,7 @@ func (gsr *GormStockRepository) Create(ctx context.Context, newStock *models.Sto
 
 	newStock.VendorId = vendorId
 
-	if err := gsr.Db.WithContext(ctx).Create(newStock).Error; err != nil {
+	if err := gsr.db.WithContext(ctx).Create(newStock).Error; err != nil {
 		return nil, utils.ErrorHandler(err, "Error creating stock")
 	}
 
@@ -68,8 +73,14 @@ func (gsr *GormStockRepository) Create(ctx context.Context, newStock *models.Sto
 
 func (gsr *GormStockRepository) UpdateStock(ctx context.Context, updatedStock *models.Stock) error {
 
-	if err := gsr.Db.WithContext(ctx).Save(updatedStock).Error; err != nil {
-		return utils.ErrorHandler(err, "Error updating stock")
+	res := gsr.db.WithContext(ctx).Save(updatedStock)
+
+	if res.Error != nil {
+		return utils.ErrorHandler(res.Error, "Error updating stock")
+	}
+
+	if res.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
 	}
 
 	return nil
@@ -78,8 +89,14 @@ func (gsr *GormStockRepository) UpdateStock(ctx context.Context, updatedStock *m
 
 func (gsr *GormStockRepository) UpdateStockProduct(ctx context.Context, updatedStockProduct *models.StocksProduct) error {
 
-	if err := gsr.Db.WithContext(ctx).Save(updatedStockProduct).Error; err != nil {
-		return utils.ErrorHandler(err, "Error updating stock product")
+	res := gsr.db.WithContext(ctx).Save(updatedStockProduct)
+
+	if res.Error != nil {
+		return utils.ErrorHandler(res.Error, "Error updating stock product")
+	}
+
+	if res.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
 	}
 
 	return nil
@@ -88,8 +105,14 @@ func (gsr *GormStockRepository) UpdateStockProduct(ctx context.Context, updatedS
 
 func (gsr *GormStockRepository) PatchStockId(ctx context.Context, modifiedStock *models.Stock) (*models.Stock, error) {
 
-	if err := gsr.Db.WithContext(ctx).Save(modifiedStock).Error; err != nil {
-		return nil, utils.ErrorHandler(err, "Error modifying stock")
+	res := gsr.db.WithContext(ctx).Save(modifiedStock)
+
+	if res.Error != nil {
+		return nil, utils.ErrorHandler(res.Error, "Error modifying stock")
+	}
+
+	if res.RowsAffected == 0 {
+		return nil, gorm.ErrRecordNotFound
 	}
 
 	return modifiedStock, nil
@@ -97,8 +120,14 @@ func (gsr *GormStockRepository) PatchStockId(ctx context.Context, modifiedStock 
 
 func (gsr *GormStockRepository) PatchStockProductId(ctx context.Context, modifiedStockProduct *models.StocksProduct) (*models.StocksProduct, error) {
 
-	if err := gsr.Db.WithContext(ctx).Save(modifiedStockProduct).Error; err != nil {
-		return nil, utils.ErrorHandler(err, "Error updating stock product")
+	res := gsr.db.WithContext(ctx).Save(modifiedStockProduct)
+
+	if res.Error != nil {
+		return nil, utils.ErrorHandler(res.Error, "Error updating stock product")
+	}
+
+	if res.RowsAffected == 0 {
+		return nil, gorm.ErrRecordNotFound
 	}
 
 	return modifiedStockProduct, nil
@@ -107,22 +136,17 @@ func (gsr *GormStockRepository) PatchStockProductId(ctx context.Context, modifie
 
 func (gsr *GormStockRepository) PatchStockProducts(ctx context.Context, modifiedStockProducts []models.StocksProduct) ([]models.StocksProduct, error) {
 
-	err := gsr.Db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+	for i := range modifiedStockProducts {
 
-		for i := range modifiedStockProducts {
+		stockProduct := &modifiedStockProducts[i]
 
-			stockProduct := &modifiedStockProducts[i]
-
-			if err := tx.Save(stockProduct).Error; err != nil {
-				return utils.ErrorHandler(err, "Error updating stock products")
-			}
+		res := gsr.db.WithContext(ctx).Save(stockProduct)
+		if res.Error != nil {
+			return nil, utils.ErrorHandler(res.Error, "Error updating stock products")
 		}
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
+		if res.RowsAffected == 0 {
+			return nil, gorm.ErrRecordNotFound
+		}
 	}
 
 	return modifiedStockProducts, nil
@@ -131,7 +155,7 @@ func (gsr *GormStockRepository) PatchStockProducts(ctx context.Context, modified
 
 func (gsr *GormStockRepository) DeleteStockById(ctx context.Context, id uuid.UUID, vendorId uuid.UUID) error {
 
-	res := gsr.Db.WithContext(ctx).Where("id = ? AND vendor_id = ?", id, vendorId).Delete(&models.Stock{})
+	res := gsr.db.WithContext(ctx).Where("id = ? AND vendor_id = ?", id, vendorId).Delete(&models.Stock{})
 	if res.Error != nil {
 		return utils.ErrorHandler(res.Error, "Error deleting stock")
 	}
@@ -147,11 +171,11 @@ func (gsr *GormStockRepository) DeleteStockProductById(ctx context.Context, stoc
 
 	var stock models.Stock
 
-	if err := gsr.Db.WithContext(ctx).First(&stock, "id = ? AND vendor_id = ?", stockId, vendorId).Error; err != nil {
+	if err := gsr.db.WithContext(ctx).First(&stock, "id = ? AND vendor_id = ?", stockId, vendorId).Error; err != nil {
 		return utils.ErrorHandler(err, "Invalid stock data")
 	}
 
-	res := gsr.Db.WithContext(ctx).Where("stock_id = ? AND product_id = ?", stockId, productId).Delete(&models.StocksProduct{})
+	res := gsr.db.WithContext(ctx).Where("stock_id = ? AND product_id = ?", stockId, productId).Delete(&models.StocksProduct{})
 	if res.Error != nil {
 		return utils.ErrorHandler(res.Error, "Error deleting stock product")
 	}
@@ -165,46 +189,38 @@ func (gsr *GormStockRepository) DeleteStockProductById(ctx context.Context, stoc
 
 func (gsr *GormStockRepository) DeleteManyStocks(ctx context.Context, ids []uuid.UUID, vendorId uuid.UUID) error {
 
-	return gsr.Db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+	res := gsr.db.WithContext(ctx).Where("vendor_id = ? AND id IN ?", vendorId, ids).Delete(&models.Stock{})
 
-		res := tx.Where("vendor_id = ? AND id IN ?", vendorId, ids).Delete(&models.Stock{})
+	if res.Error != nil {
+		return utils.ErrorHandler(res.Error, "Error deleting stock")
+	}
 
-		if res.Error != nil {
-			return utils.ErrorHandler(res.Error, "Error deleting stock")
-		}
+	if res.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
 
-		if res.RowsAffected == 0 {
-			return gorm.ErrRecordNotFound
-		}
-
-		return nil
-
-	})
+	return nil
 
 }
 
 func (gsr *GormStockRepository) DeleteManyStockProducts(ctx context.Context, ids []uuid.UUID, stockId uuid.UUID, vendorId uuid.UUID) error {
 
-	return gsr.Db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+	var stock models.Stock
+	if err := gsr.db.WithContext(ctx).First(&stock, "id = ? AND vendor_id = ?", stockId, vendorId).Error; err != nil {
+		return utils.ErrorHandler(err, "Invalid stock data")
+	}
 
-		var stock models.Stock
-		if err := tx.First(&stock, "id = ? AND vendor_id = ?", stockId, vendorId).Error; err != nil {
-			return utils.ErrorHandler(err, "Invalid stock data")
-		}
+	res := gsr.db.WithContext(ctx).Where("stock_id = ? AND product_id IN ?", stockId, ids).Delete(&models.StocksProduct{})
 
-		res := tx.Where("stock_id = ? AND product_id IN ?", stockId, ids).Delete(&models.StocksProduct{})
+	if res.Error != nil {
+		return utils.ErrorHandler(res.Error, "Error deleting stock products")
+	}
 
-		if res.Error != nil {
-			return utils.ErrorHandler(res.Error, "Error deleting stock products")
-		}
+	if res.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
 
-		if res.RowsAffected == 0 {
-			return gorm.ErrRecordNotFound
-		}
-
-		return nil
-
-	})
+	return nil
 
 }
 
@@ -212,7 +228,7 @@ func (gsr *GormStockRepository) CheckProduct(ctx context.Context, productId uuid
 
 	var product productsModels.Product
 
-	if err := gsr.Db.WithContext(ctx).First(&product, "id = ? AND vendor_id = ?", productId, vendorId).Error; err != nil {
+	if err := gsr.db.WithContext(ctx).First(&product, "id = ? AND vendor_id = ?", productId, vendorId).Error; err != nil {
 		return utils.ErrorHandler(err, "Error: no such product")
 	}
 
@@ -224,10 +240,50 @@ func (gsr *GormStockRepository) CheckLocation(ctx context.Context, locationId uu
 
 	var location models.StocksLocation
 
-	if err := gsr.Db.WithContext(ctx).First(&location, "id = ?", locationId).Error; err != nil {
+	if err := gsr.db.WithContext(ctx).First(&location, "id = ?", locationId).Error; err != nil {
 		return nil, utils.ErrorHandler(err, "Error: no such location")
 	}
 
 	return &location, nil
+
+}
+
+func (gsr *GormStockRepository) FindProductStocksQuantities(ctx context.Context, productId uuid.UUID, vendorId uuid.UUID) ([]models.StocksProduct, error) {
+
+	var productStocks []models.StocksProduct
+
+	if err := gsr.db.WithContext(ctx).Where("product_id = ?", productId).Preload("Stock", "vendor_id = ?", vendorId).Find(&productStocks).Error; err != nil {
+		return nil, utils.ErrorHandler(err, "Error: no such products in stocks")
+	}
+
+	return productStocks, nil
+
+}
+
+func (gsr *GormStockRepository) WithTx(tx *gorm.DB) domain.StockRepository {
+	return &GormStockRepository{
+		db: tx,
+	}
+}
+
+func (gsr *GormStockRepository) Transaction(fn func(txRepo domain.StockRepository) error) error {
+
+	tx := gsr.db.Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	repo := gsr.WithTx(tx)
+
+	if err := fn(repo); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return err
+	}
+
+	return nil
 
 }
