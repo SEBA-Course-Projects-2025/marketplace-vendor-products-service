b00f1e0fa31a90ae22fe39ad9e316fc5f509ede4
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-21 00:31:03 +0300

    fix: updated hanlder struct

--- product-service/internal/shared/handler/handler_struct.go
+++ product-service/internal/shared/handler/handler_struct.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	productDomain "dev-vendor/product-service/internal/products/domain"
 	stockDomain "dev-vendor/product-service/internal/stocks/domain"
+	"gorm.io/gorm"
 )
 
 type Handler struct {
-
 	ProductRepo productDomain.ProductRepository
 
 	StockRepo stockDomain.StockRepository
+
+	Db *gorm.DB
 }
