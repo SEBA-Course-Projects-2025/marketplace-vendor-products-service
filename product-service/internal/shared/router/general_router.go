0bd1e7118806a2108c729fcaf057cdfc8db04532
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-22 16:46:37 +0300

    feat: updated general router file to support swagger annotations

--- product-service/internal/shared/router/general_router.go
+++ product-service/internal/shared/router/general_router.go
@@ -1,17 +1,22 @@
 package router
 
 import (
+	_ "dev-vendor/docs"
 	productInterfaces "dev-vendor/product-service/internal/products/interfaces"
 	productsHandlers "dev-vendor/product-service/internal/products/interfaces/handlers"
 	"dev-vendor/product-service/internal/shared/middlewares"
 	stocksInterfaces "dev-vendor/product-service/internal/stocks/interfaces"
 	stockHandlers "dev-vendor/product-service/internal/stocks/interfaces/handlers"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetUpRouter(productHandler *productsHandlers.ProductHandler, stockHandler *stockHandlers.StockHandler) *gin.Engine {
 
 	r := gin.New()
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	api := r.Group("/api", middlewares.VendorIdMiddleware())
 
