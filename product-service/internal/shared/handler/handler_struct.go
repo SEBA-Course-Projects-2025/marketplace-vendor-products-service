724a1792a456ed16ce52ff78350b78861764dc3c
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-03 22:22:13 +0300

    refactor: updated handler struct

--- product-service/internal/shared/handler/handler_struct.go
+++ product-service/internal/shared/handler/handler_struct.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	eventDomain "dev-vendor/product-service/internal/event/domain"
 	productDomain "dev-vendor/product-service/internal/products/domain"
 	stockDomain "dev-vendor/product-service/internal/stocks/domain"
 	"gorm.io/gorm"
@@ -11,8 +10,6 @@ type Handler struct {
 	ProductRepo productDomain.ProductRepository
 
 	StockRepo stockDomain.StockRepository
-
-	EventRepo eventDomain.EventRepository
 
 	Db *gorm.DB
 }
