021f4197566007ea9ad75da0f30a76ed4f15278c
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-10 17:22:57 +0300

    feat: added method signatures for finding a prodyct by slug, finding all tags, deleting product by slug and deleting current products images and tags

--- product-service/internal/products/domain/repository.go
+++ product-service/internal/products/domain/repository.go
@@ -10,11 +10,16 @@ import (
 
 type ProductRepository interface {
 	FindById(ctx context.Context, id uuid.UUID) (*productModels.Product, error)
+	FindBySlug(ctx context.Context, slug string, vendorId uuid.UUID) (*productModels.Product, error)
 	FindAll(ctx context.Context, params dtos.ProductQueryParams, vendorId uuid.UUID) ([]productModels.Product, error)
+	FindAllTags(ctx context.Context) ([]productModels.Tag, error)
 	Create(ctx context.Context, newProduct *productModels.Product, vendorId uuid.UUID) (*productModels.Product, error)
 	Update(ctx context.Context, updatedProduct *productModels.Product) error
 	Patch(ctx context.Context, modifiedProduct *productModels.Product) (*productModels.Product, error)
 	DeleteById(ctx context.Context, id uuid.UUID, vendorId uuid.UUID) error
+	DeleteBySlug(ctx context.Context, slug string, vendorId uuid.UUID) error
+	DeleteProductImages(ctx context.Context, product *productModels.Product) error
+	DeleteProductTags(ctx context.Context, product *productModels.Product) error
 	DeleteMany(ctx context.Context, ids []uuid.UUID, vendorId uuid.UUID) error
 	Transaction(fn func(txRepo ProductRepository) error) error
 	WithTx(tx *gorm.DB) ProductRepository
