d9c7d217061b6b47af909ffdf94deb883d919a61
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-10 17:21:53 +0300

    feat: updated products models with new fields

--- product-service/internal/products/domain/productModels/product.go
+++ product-service/internal/products/domain/productModels/product.go
@@ -18,15 +18,21 @@ type Product struct {
 	Quantity    int             `json:"quantity" gorm:"column:quantity;type:int;not null"`
 	CreatedAt   time.Time       `gorm:"column:created_at;type:timestamp"`
 	UpdatedAt   time.Time       `gorm:"column:updated_at;type:timestamp"`
+	DeletedAt   *time.Time      `gorm:"column:deleted_at;type:timestamp"`
 }
 
 type ProductsImage struct {
-	Id        uuid.UUID `json:"id" gorm:"column:id;type:uuid;primary key;not null"`
+	Id        uuid.UUID `json:"id" gorm:"column:id;type:uuid;primaryKey;not null"`
 	ImageUrl  string    `json:"image_url" gorm:"column:image_url;type:text;not null"`
 	ProductId uuid.UUID `json:"product_id" gorm:"column:product_id;type:uuid;not null"`
 }
 
 type Tag struct {
-	Id      uuid.UUID `json:"id" gorm:"column:id;type:uuid;primary key;not null"`
-	TagName string    `json:"tag_name" gorm:"colum:tag_name;type:text;not null"`
+	Id      uuid.UUID `json:"id" gorm:"column:id;type:uuid;primaryKey;not null"`
+	TagName string    `json:"tag_name" gorm:"column:tag_name;type:text;not null"`
+}
+
+type ProductsTag struct {
+	ProductId uuid.UUID `gorm:"column:product_id;type:uuid;primaryKey"`
+	TagId     uuid.UUID `gorm:"column:tag_id;type:uuid;primaryKey"`
 }
