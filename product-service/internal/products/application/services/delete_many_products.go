a242109235b541f340ce6a8f1b43ac0c2f86bc6e
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-10 17:20:55 +0300

    feat: added support for event of deleting the product

--- product-service/internal/products/application/services/delete_many_products.go
+++ product-service/internal/products/application/services/delete_many_products.go
@@ -2,18 +2,40 @@ package services
 
 import (
 	"context"
+	eventDomain "dev-vendor/product-service/internal/event/domain"
 	"dev-vendor/product-service/internal/products/domain"
+	"dev-vendor/product-service/internal/products/dtos"
 	"dev-vendor/product-service/internal/shared/tracer"
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
-func DeleteManyProducts(ctx context.Context, repo domain.ProductRepository, ids []uuid.UUID, vendorId uuid.UUID) error {
+func DeleteManyProducts(ctx context.Context, repo domain.ProductRepository, eventRepo eventDomain.EventRepository, db *gorm.DB, ids []uuid.UUID, vendorId uuid.UUID) error {
 
 	ctx, span := tracer.Tracer.Start(ctx, "DeleteAllProducts")
 	defer span.End()
 
-	return repo.Transaction(func(txRepo domain.ProductRepository) error {
-		return txRepo.DeleteMany(ctx, ids, vendorId)
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		txProductRepo := repo.WithTx(tx)
+		txEventRepo := eventRepo.WithTx(tx)
+
+		if err := txProductRepo.DeleteMany(ctx, ids, vendorId); err != nil {
+			return err
+		}
+
+		outbox, err := dtos.DeletedProductToOutbox(ids, "product.deleted.catalog", "product.catalog.events")
+
+		if err != nil {
+			return err
+		}
+
+		err = txEventRepo.CreateOutboxRecord(ctx, outbox)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
 	})
 
 }
--- product-service/internal/products/application/services/delete_product_by_id.go
+++ product-service/internal/products/application/services/delete_product_by_id.go
@@ -2,18 +2,42 @@ package services
 
 import (
 	"context"
+	eventDomain "dev-vendor/product-service/internal/event/domain"
 	"dev-vendor/product-service/internal/products/domain"
+	"dev-vendor/product-service/internal/products/dtos"
 	"dev-vendor/product-service/internal/shared/tracer"
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
-func DeleteProductById(ctx context.Context, repo domain.ProductRepository, id uuid.UUID, vendorId uuid.UUID) error {
+func DeleteProductById(ctx context.Context, repo domain.ProductRepository, eventRepo eventDomain.EventRepository, db *gorm.DB, id uuid.UUID, vendorId uuid.UUID) error {
 
-	ctx, span := tracer.Tracer.Start(ctx, "DeleteOneProduct")
+	ctx, span := tracer.Tracer.Start(ctx, "DeleteOneProductById")
 	defer span.End()
 
-	return repo.Transaction(func(txRepo domain.ProductRepository) error {
-		return txRepo.DeleteById(ctx, id, vendorId)
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+
+		txProductRepo := repo.WithTx(tx)
+		txEventRepo := eventRepo.WithTx(tx)
+
+		if err := txProductRepo.DeleteById(ctx, id, vendorId); err != nil {
+			return err
+		}
+
+		outbox, err := dtos.DeletedProductToOutbox([]uuid.UUID{id}, "product.deleted.catalog", "product.catalog.events")
+
+		if err != nil {
+			return err
+		}
+
+		err = txEventRepo.CreateOutboxRecord(ctx, outbox)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+
 	})
 
 }
