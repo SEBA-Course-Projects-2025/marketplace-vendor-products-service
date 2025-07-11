d6b1895f832409e933e5c7b1ea8be769b2da6f45
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-11 10:21:35 +0300

    feat: updated geenral router to support metrics endpoint

--- product-service/internal/shared/router/general_router.go
+++ product-service/internal/shared/router/general_router.go
@@ -9,20 +9,33 @@ import (
 	stockHandlers "dev-vendor/product-service/internal/stocks/interfaces/handlers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	ginprometheus "github.com/zsais/go-gin-prometheus"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"log"
+	"os"
 )
 
 func SetUpRouter(productHandler *productsHandlers.ProductHandler, stockHandler *stockHandlers.StockHandler) *gin.Engine {
 
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	r := gin.New()
 
-	p := ginprometheus.NewPrometheus("vendor-product-service")
+	p := ginprometheus.NewPrometheus("vendor_product_service")
+	p.MetricsPath = ""
 	p.Use(r)
 
-	r.Use(otelgin.Middleware("vendor-product-service"))
+	r.GET("/metrics", gin.BasicAuth(gin.Accounts{
+		os.Getenv("METRICS_ACCESS_USERNAME"): os.Getenv("METRICS_ACCESS_PASSWORD"),
+	}), gin.WrapH(promhttp.Handler()))
+
+	r.Use(otelgin.Middleware("vendor_product_service"))
 
 	r.Use(cors.New(cors.Config{
 		AllowAllOrigins: true,
