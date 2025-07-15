beff70f863fded7918944e0ae036bece132503a5
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-15 11:53:54 +0300

    feat: added support of logrus logs for GetAllProducts application service

--- product-service/internal/products/application/services/get_all_products.go
+++ product-service/internal/products/application/services/get_all_products.go
@@ -6,9 +6,14 @@ import (
 	"dev-vendor/product-service/internal/products/dtos"
 	"dev-vendor/product-service/internal/shared/tracer"
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
 func GetAllProducts(ctx context.Context, repo domain.ProductRepository, params dtos.ProductQueryParams, vendorId uuid.UUID) ([]dtos.GetProductsResponse, error) {
+
+	logrus.WithFields(logrus.Fields{
+		"vendorId": vendorId,
+	}).Info("Starting GetAllProducts application service")
 
 	ctx, span := tracer.Tracer.Start(ctx, "GetAllProducts")
 	defer span.End()
@@ -19,6 +24,9 @@ func GetAllProducts(ctx context.Context, repo domain.ProductRepository, params d
 		return nil, err
 	}
 
+	logrus.WithFields(logrus.Fields{
+		"vendorId": vendorId,
+	}).Info("Successfully get paginated list of products")
 	return dtos.ProductsToDto(products), nil
 
 }
